d9b1b53547e07f3916e177210ac75642ecb49589
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-06-29 15:10:58 +0500

    Getting Data from API using API Key

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+# API Reference = "https://openweathermap.org/current"
+
+import requests
+OpenWeather_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
+api_key = "YOUR_API_KEY_HERE"
+
+weather_params ={
+    "lat":  33.6995,
+    "lon": 73.0363,
+    "appid":api_key
+}
+
+responce = requests.get(OpenWeather_Endpoint,params=weather_params)
+
+print(responce.status_code)
+print(responce.json())
